fca8ce814bd9c9b4b4ef5b1d22f88bf905e7fc9e
nomui68-hub <nomui68@gmail.com>
2026-06-15 16:19:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Cấu hình kết nối Google Sheets.
 // Sau khi triển khai Apps Script dạng Web App, dán URL vào dòng dưới.
 // Ví dụ: window.EXAM_API_URL = 'https://script.google.com/macros/s/XXXXX/exec';
-window.EXAM_API_URL = 'https://script.google.com/macros/s/AKfycbzU3W24pkMemp-53MqFusNb-3eVFEK51BDz_LqoFZCS0P0i3OQH0tTb-hQLADXurue/exec';
+EXAM_API_URL = 'https://script.google.com/macros/s/AKfycbzU3W24pkMemp-53MqFusNb-3eVFEK51BDz_LqoFZCS0P0i3OQH0tTb-hQLADXurue/exec';
