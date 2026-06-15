dab4c12cb768a1ca60db95077a0515379350f0a8
nomui68-hub <nomui68@gmail.com>
2026-06-15 17:22:44 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 // Cấu hình kết nối Google Sheets.
 // Sau khi triển khai Apps Script dạng Web App, dán URL vào dòng dưới.
 // Ví dụ: window.EXAM_API_URL = 'https://script.google.com/macros/s/XXXXX/exec';
-https://script.google.com/macros/s/AKfycbyectFKP6gp3BB5G2Tho8xWxiCtnJ5mAO7aA0cQAlU2xPRGEwiURel4M1iqB3Z_f-rA/exec
+window.EXAM_API_URL =
+'https://script.google.com/macros/s/AKfycbzZSLHoLn4maS56NRozYge0CWhkX7Sd6X_FwiiXzLBCopZTQao7JafxtWZ-AMlJikx6/exec';
